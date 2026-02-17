1624841d0e92b0e60248f00401f2696d07edc71a
SwaroopDangal <swaroopdangal732@gmail.com>
2026-02-17 11:20:45 +0545

    learnt abut different find queries

--- index.ts
+++ index.ts
@@ -1,9 +1,50 @@
 import express from "express";
+import { prisma } from "./db";
 
 const app = express();
 app.use(express.json());
 
+// Query to fetch all married users above the age of 32
+// app.get("/users", async (req, res) => {
+//   const users = await prisma.user.findMany({
+//     where: { isMarried: true, age: { gt: 32 } },
+//   });
+//   res.json(users);
+// });
+
+// Query to fetch married or nepali users
+
+// app.get("/users", async (req, res) => {
+//   const users = await prisma.user.findMany({
+//     where: {
+//       OR: [{ isMarried: true }, { nationality: "Nepali" }],
+//     },
+//   });
+//   res.json(users);
+// });
+
+// WHO are not nepali
+// app.get("/users", async (req, res) => {
+//   const users = await prisma.user.findMany({
+//     where: {
+//       nationality: { not: "Nepali" },
+//     },
+//   });
+//   res.json(users);
+// });
+
+// Nationality nepal or indina
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany({
+    where: {
+      nationality: {
+        in: ["Nepali", "Indian"],
+      },
+    },
+  });
+  res.json(users);
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,11 +11,41 @@ const prisma = new PrismaClient({ adapter });
 async function seed() {
   await prisma.user.createMany({
     data: [
-      { name: "Swaroop Dangal", email: "swaroop@example.com" },
-      { name: "Aayush Sharma", email: "aayush@example.com" },
-      { name: "Sita Karki", email: "sita@example.com" },
-      { name: "Ram Thapa", email: "ram@example.com" },
-      { name: "Anisha Gurung", email: "anisha@example.com" },
+      {
+        name: "Swaroop Dangal",
+        email: "swaroop@example.com",
+        nationality: "Nepali",
+        isMarried: false,
+        age: 23,
+      },
+      {
+        name: "Aayush Sharma",
+        email: "aayush@example.com",
+        nationality: "Indian",
+        isMarried: false,
+        age: 27,
+      },
+      {
+        name: "Sita Karki",
+        email: "sita@example.com",
+        nationality: "Nepali",
+        isMarried: true,
+        age: 31,
+      },
+      {
+        name: "Ram Thapa",
+        email: "ram@example.com",
+        nationality: "Nepali",
+        isMarried: true,
+        age: 35,
+      },
+      {
+        name: "Anisha Gurung",
+        email: "anisha@example.com",
+        nationality: "Nepali",
+        isMarried: false,
+        age: 24,
+      },
     ],
   });
 }
